f950a0db7670a2a2e7a89cf5bd71368d410bb9ce
Amanda Ramos <46381127+ramosamanda@users.noreply.github.com>
2019-01-04 11:56:45 -0500

    Add files via upload

---
+++ CSCI-220L-F18-Lab9-McDonald.py
@@ -0,0 +1,23 @@
+# CSCI220L-F18-Lab9-McDonald-APR.py
+# Amanda Ramos
+# A program to sing OldMcDonald.
+
+def song(animal, sound):
+    print("Old Mac Donald had a farm, Ee-igh, Ee-igh, Oh!")
+    print("And on that farm he had a", animal,"Ee-igh, Ee-igh, Oh!")
+    print("With a", sound , "," , sound , "here and a", sound , ",", sound, "there.")
+    print("Here a" , sound , "," , "there a" , sound , "," , "everywhere a" , sound , "," , sound , ".")
+    print("Old Mac Donald had a farm, Ee-igh, Ee-igh, Oh!")
+    print()
+          
+
+def main():
+    song("cow", "moo")
+    song("pig", "oink")
+    song("horse", "neigh")
+
+main()
+    
+
+    
+    
